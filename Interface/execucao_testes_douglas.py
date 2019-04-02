0249c0cbcff9acc67c1b94fcf879e916c2cdc9ba
douglasscorrea <dscorrea@inf.ufpel.edu.br>
2019-04-02 14:57:43 -0300

    atualizando script de execucao

--- Interface/execucao_testes_douglas.py
+++ Interface/execucao_testes_douglas.py
@@ -7,12 +7,51 @@ import a_star
 import utils
 
 board_size = 3
-test_number = 10
+test_number = 25
 shuffle_number = [10, 50, 100, 250, 500, 1000]
 shuffler = shuffle.Shuffle(board_size)
+boards = [[0 for x in range(test_number)] for y in range(len(shuffle_number))] 
+
+def generate_boards():
+	for _ in range(30):
+		shuffler.shuffle_algorithm(10)
+		boards10.append(shuffler.get_matrix())
+
+	for _ in range(30):
+		shuffler.shuffle_algorithm(50)
+		boards50.append(shuffler.get_matrix())
+
+	for _ in range(30):
+		shuffler.shuffle_algorithm(100)
+		boards100.append(shuffler.get_matrix())
+
+	for _ in range(30):
+		shuffler.shuffle_algorithm(250)
+		boards250.append(shuffler.get_matrix())
+
+	for _ in range(30):
+		shuffler.shuffle_algorithm(500)
+		boards500.append(shuffler.get_matrix())
+
+	for _ in range(30):
+		shuffler.shuffle_algorithm(1000)
+		boards1000.append(shuffler.get_matrix())
+
+	print(len(boards10))
+	print(len(boards50))
+	print(len(boards100))
+	print(len(boards250))
+	print(len(boards500))
+	print(len(boards1000))
+def generate_boards2():
+	for i in range(len(shuffle_number)):
+		for j in range(test_number):
+			shuffler = shuffle.Shuffle(board_size)
+			shuffler.shuffle_algorithm(shuffle_number[i])
+			boards[i][j] = shuffler.get_matrix()
 
 def test_bfs():
-	with open('/Users/douglascorrea/FIA/BFS.csv', 'wb') as file:
+	with open('/home/douglascorrea/FIA/BFS.csv', 'wb') as file:
 		for moves_number in shuffle_number:
 			file.write(str(moves_number) + '\n')
 			file.write(' ' + ',')
@@ -22,6 +61,7 @@ def test_bfs():
 			average_time = 0
 			average_memory = 0
 			for i in range (0, test_number):
+				print(i)
 				shuffler.shuffle_algorithm(moves_number)
 				bfs_alg = bfs.BFS(shuffler.get_matrix(), board_size)
 
@@ -41,7 +81,7 @@ def test_bfs():
 				average_memory += m
 			file.write(str(average_memory/10) + '\n')
 def test_dfs():
-	with open('/Users/douglascorrea/FIA/DFS.csv', 'wb') as file:
+	with open('/home/douglascorrea/FIA/DFS.csv', 'wb') as file:
 		for moves_number in shuffle_number:
 			file.write(str(moves_number) + '\n')
 			file.write(' ' + ',')
@@ -51,6 +91,7 @@ def test_dfs():
 			average_time = 0
 			average_memory = 0
 			for i in range (0, test_number):
+				print(i)
 				shuffler.shuffle_algorithm(moves_number)
 				dfs_alg = dfs.DFS(shuffler.get_matrix(), board_size)
 
@@ -70,7 +111,7 @@ def test_dfs():
 				average_memory += m
 			file.write(str(average_memory/10) + '\n')
 def test_idfs():
-	with open('/Users/douglascorrea/FIA/iDFS.csv', 'wb') as file:
+	with open('/home/douglascorrea/FIA/iDFS.csv', 'wb') as file:
 		for moves_number in shuffle_number:
 			file.write(str(moves_number) + '\n')
 			file.write(' ' + ',')
@@ -80,7 +121,9 @@ def test_idfs():
 			average_time = 0
 			average_memory = 0
 			for i in range (0, test_number):
+				print(i)
 				shuffler.shuffle_algorithm(moves_number)
+				print(shuffler.get_matrix())
 				dfs_it_alg = it_dfs.IT_DFS(shuffler.get_matrix(), board_size)
 
 				start = time.time()
@@ -99,7 +142,7 @@ def test_idfs():
 				average_memory += m
 			file.write(str(average_memory/10) + '\n')
 def test_a_star1():
-	with open('/Users/douglascorrea/FIA/a_star1.csv', 'wb') as file:
+	with open('/home/douglascorrea/FIA/a_star1.csv', 'wb') as file:
 		for moves_number in shuffle_number:
 			file.write(str(moves_number) + '\n')
 			file.write(' ' + ',')
@@ -109,11 +152,13 @@ def test_a_star1():
 			average_time = 0
 			average_memory = 0
 			for i in range (0, test_number):
+				print(i)
 				shuffler.shuffle_algorithm(moves_number)
+				print(shuffler.get_matrix())
 				a_star_alg = a_star.A_STAR(shuffler.get_matrix(), board_size)
 
 				start = time.time()
-				a_star_alg.a_star_algorithm(utils.diff_heuristic)
+				a_star_alg.a_star_algorithm(utils.chessboard_heuristic)
 				end = time.time()
 
 				times.append(end - start)
@@ -128,19 +173,17 @@ def test_a_star1():
 				average_memory += m
 			file.write(str(average_memory/10) + '\n')
 def test_a_star2():
-	with open('/Users/douglascorrea/FIA/a_star2.csv', 'wb') as file:
-		for moves_number in shuffle_number:
-			file.write(str(moves_number) + '\n')
-			file.write(' ' + ',')
-			print("Moves Number: " + str(moves_number))
-			times = []
-			memory = []
-			average_time = 0
-			average_memory = 0
-			for i in range (0, test_number):
-				shuffler.shuffle_algorithm(moves_number)
-				a_star_alg = a_star.A_STAR(shuffler.get_matrix(), board_size)
-
+	with open('/home/douglascorrea/FIA/a_star2_test.csv', 'wb') as file:
+		times = []
+		memory = []
+		average_time = 0
+		average_memory = 0
+		for i in range(len(shuffle_number)):
+			print("Moves: " + str(shuffle_number[i]))
+			for j in range(test_number):
+				print(j)
+				print(boards[i][j])
+				a_star_alg = a_star.A_STAR(boards[i][j], board_size)
 				start = time.time()
 				a_star_alg.a_star_algorithm(utils.manhattan_heuristic)
 				end = time.time()
@@ -157,9 +200,10 @@ def test_a_star2():
 				average_memory += m
 			file.write(str(average_memory/10) + '\n')
 
-test_bfs()
-#test_dfs()
-#test_idfs()
+generate_boards2()
+test_a_star2()
+#test_a_star2() RODOU
 #test_a_star1()
-#test_a_star2()
-
+#test_dfs() RODOU
+#test_idfs()
+#test_bfs()
